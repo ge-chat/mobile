46b59af2de8e4c7e86858081233cbcdf0f37bbc3
Tarnenak Yauheni <tarnenok@gmail.com>
2017-04-28 21:38:53 -0800

    restriction if user is not in chat

--- app/src/main/java/by/tarnenok/geofy/ChartActivity.kt
+++ app/src/main/java/by/tarnenok/geofy/ChartActivity.kt
@@ -12,16 +12,16 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
+import android.view.View
 import android.widget.EditText
 import by.tarnenok.geofy.services.SignalRService
 import by.tarnenok.geofy.services.TokenService
 import by.tarnenok.geofy.services.api.*
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationServices
 import com.google.gson.Gson
 import microsoft.aspnet.signalr.client.hubs.HubConnection
-import org.jetbrains.anko.alert
-import org.jetbrains.anko.find
-import org.jetbrains.anko.onClick
-import org.jetbrains.anko.toast
+import org.jetbrains.anko.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -36,6 +36,13 @@ class ChartActivity : AppCompatActivity(){
 
     var chartModel: ChartReadModel? = null
     var signalrConnection: HubConnection? = null
+    var mApiClient: GoogleApiClient? = null
+
+    var sendMessageButton: com.rey.material.widget.ImageButton? = null
+    var sendMessageText: EditText? = null
+    var sendMessageView: View? = null
+
+    var inChat = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -62,8 +69,10 @@ class ChartActivity : AppCompatActivity(){
             true
         };
 
-        val sendMessageButton = find<com.rey.material.widget.ImageButton>(R.id.button_send)
-        sendMessageButton.onClick {
+        sendMessageView = find<View>(R.id.view_send)
+        sendMessageText = find<EditText>(R.id.edit_message)
+        sendMessageButton = find<com.rey.material.widget.ImageButton>(R.id.button_send)
+        sendMessageButton?.onClick {
             val message = find<EditText>(R.id.edit_message).text.toString()
             ApiService.message.create(SendMessageModel(message, chartModel!!.id)).enqueue(
                 object : Callback<Void> {
@@ -91,8 +100,8 @@ class ChartActivity : AppCompatActivity(){
         chartHub.on(SignalRService.Hubs.Chart.MessagePosted, { data -> handler.post {
             chartModel!!.messages.add(data)
             messagesRV.adapter.notifyDataSetChanged()
-            val message = find<EditText>(R.id.edit_message)
-            message.text.clear()
+            sendMessageText = find<EditText>(R.id.edit_message)
+            sendMessageText!!.text.clear()
             messagesRV.smoothScrollToPosition(messagesRV.adapter.itemCount - 1)
         }}, MessageReadModel::class.java)
         chartHub.on(SignalRService.Hubs.Chart.ParticipantAdded, { data ->
@@ -100,15 +109,63 @@ class ChartActivity : AppCompatActivity(){
                 chartModel!!.participants.add(data.participant)
             }
         }, ParticipantAddedSignal::class.java)
+
+        mApiClient = GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(object : GoogleApiClient.ConnectionCallbacks{
+                    override fun onConnectionSuspended(p0: Int) { }
+
+                    override fun onConnected(bundle: Bundle?) {
+                        LocationServices.FusedLocationApi.requestLocationUpdates(
+                                mApiClient, Config.locationRequest) { location ->
+                            checkPosition(location)
+                        };
+                    }
+                })
+                .addOnConnectionFailedListener { }
+                .addApi(LocationServices.API)
+                .build()
     }
 
     override fun onStart() {
         signalrConnection?.start()
+        mApiClient?.connect()
         super.onStart()
     }
 
     override fun onStop() {
         signalrConnection?.stop()
+        mApiClient?.disconnect()
         super.onStop()
+    }
+
+
+    fun checkPosition(location: android.location.Location) {
+        if(chartModel == null) return
+
+        val fromLocation = android.location.Location("")
+        fromLocation.latitude = chartModel!!.location.longitude
+        fromLocation.longitude = chartModel!!.location.latitude
+        fromLocation.altitude = 0.0
+
+        val toLocation = android.location.Location("")
+        toLocation.latitude = location.latitude
+        toLocation.longitude = location.longitude
+        toLocation.altitude = 0.0
+
+        val distance = fromLocation.distanceTo(toLocation)
+        val nowInChat = chartModel!!.radius > distance
+        if(nowInChat != inChat){
+            changeStatus(nowInChat);
+            inChat = nowInChat
+        }
+    }
+
+    fun changeStatus(inChat: Boolean){
+        sendMessageText?.isEnabled = inChat
+        sendMessageButton?.isEnabled = inChat
+        val message = if (inChat) getString(R.string.in_chat) else getString(R.string.not_in_chat)
+        toast(message)
+        val color = if(inChat) resources.getColor(R.color.white) else resources.getColor(R.color.disabled)
+        sendMessageView?.backgroundColor = color
     }
 }
