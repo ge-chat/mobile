c7435a5811321bf8540873e2e53ad38c4cc35e95
Tarnenak Yauheni <tarnenok@gmail.com>
2017-04-28 21:38:53 -0800

    some fixes

--- app/src/main/java/by/tarnenok/geofy/LoginActivity.kt
+++ app/src/main/java/by/tarnenok/geofy/LoginActivity.kt
@@ -1,11 +1,20 @@
 package by.tarnenok.geofy
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import org.jetbrains.anko.find
 
 class LoginActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        var loginButton = find<Button>(R.id.button_login);
+        loginButton.setOnClickListener({v ->
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        });
     }
 }
